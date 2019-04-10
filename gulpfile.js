8de5856937d4c9405638f06b5605f9cfd9d00039
Aiden Wallis <aiden@aidenwallis.co.uk>
2019-04-10 16:58:03 +0100

    Fix gulp task to actually copy contents of _headers into dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,17 @@ const gulp = require("gulp");
 const jsonminify = require("gulp-jsonminify");
 
 // Minify JSON to save on file size.
-gulp.task("build", () => {
-  return gulp.src(["./src/**/*.json", "./public/*"])
+gulp.task("minify", () => {
+  return gulp.src(["./src/**/*.json"])
     .pipe(jsonminify())
     .pipe(gulp.dest("dist"));
 });
+
+gulp.task("copy-public", () => {
+  return gulp.src(["./public/*"])
+    .pipe(gulp.dest("dist"));
+});
+
+
+// Minify JSON, put into dist then copy over everything else.
+gulp.task("build", gulp.series("minify", "copy-public"));
