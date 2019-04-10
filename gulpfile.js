6a5eb1680a0dc469584f151dd9cbe225e49403d3
Aiden Wallis <aiden@aidenwallis.co.uk>
2019-04-10 16:51:24 +0100

    Add CORS headers, and copying public folder into dist folder in gulp build.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const jsonminify = require("gulp-jsonminify");
 
 // Minify JSON to save on file size.
 gulp.task("build", () => {
-  return gulp.src(["./src/**/*.json"])
+  return gulp.src(["./src/**/*.json", "./public/*"])
     .pipe(jsonminify())
     .pipe(gulp.dest("dist"));
 });
