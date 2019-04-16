3cc1876596ce6923c6b4b50fde85608664babcc8
Aiden Wallis <aiden@aidenwallis.co.uk>
2019-04-16 01:40:23 +0100

    Add easy static server for development

---
+++ server.js
@@ -0,0 +1,18 @@
+/**
+ * Development Server
+ * ------------------------------------------------------
+ * Very basic static file server used during Fossabot development to test out new translations
+ * locally, so we don't have to deploy them. Safe to ignore this, unless you find a use for it.
+ */
+const handler = require("serve-handler");
+const http = require('http');
+
+const config = {
+  public: "src",
+  directoryListing: false,
+};
+
+const server = http.createServer((req, res) => handler(req, res, config));
+
+const port = process.env.PORT || 6500;
+server.listen(6500, () => console.log(`Development server started on port ${port}`));
